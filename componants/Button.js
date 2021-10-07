f61bca711d45aab670f43aff841296a63bbc1320
Aikru <urkia.p@free.fr>
2021-10-07 12:15:37 +0200

    [component/Button] add outline props

--- componants/Button.js
+++ componants/Button.js
@@ -2,31 +2,40 @@ import React from 'react'
 import { TouchableOpacity, Text, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
-export default function CustomButton({ outlined, link, children }) {
+export default function CustomButton(
+    { outlined, link, color, children } = { outlined: false, color: 'white' }
+) {
     const navigation = useNavigation()
+
+    function getoutStyle(outlined, color) {
+        console.log(outlined, color)
+        if (outlined) {
+            return {
+                borderRadius: 8,
+                alignItems: 'center',
+                padding: 10,
+                borderWidth: 1,
+                borderColor: color,
+            }
+        } else {
+            return {
+                alignItems: 'center',
+                backgroundColor: color,
+                padding: 10,
+                borderRadius: 8,
+            }
+        }
+    }
 
     const press = () => {
         navigation.navigate(link)
     }
 
     return (
-        <TouchableOpacity onPress={press} style={styles.button}>
+        <TouchableOpacity onPress={press} style={getoutStyle(outlined, color)}>
             {children}
         </TouchableOpacity>
     )
 }
 
-const styles = StyleSheet.create({
-    button: {
-        borderRadius: 5,
-    },
-    full: {
-        backgroundColor: 'red',
-        color: 'white',
-    },
-    outlined: {
-        color: 'red',
-        borderColor: 'red',
-        borderWidth: 2,
-    },
-})
+const styles = StyleSheet.create({})
