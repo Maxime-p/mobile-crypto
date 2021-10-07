eb95b2ef3a1e1939cbc4ec3cacccc55e92d48116
Aikru <urkia.p@free.fr>
2021-10-07 18:39:14 +0200

    [component/title.js] Add typo props

--- componants/ui/Title.js
+++ componants/ui/Title.js
@@ -3,23 +3,29 @@ import { Button, StyleSheet, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-function getTextStyle(color, font, fontWeight) {
+function getTextStyle(color, font, fontWeight, fontfamilly) {
     return {
         color: color,
         fontSize: font,
         textAlign: 'justify',
         fontWeight: fontWeight,
+        fontfamilly: fontfamilly,
     }
 }
 
 export default function CustomTitle(
-    { color, font, fontWeight, text } = {
+    { color, font, fontWeight, text, fontfamilly } = {
         color: '#071D48',
         font: 16,
         fontWeight: 'medium',
+        fontfamilly: 'Helvetica Neue',
     }
 ) {
-    return <Text style={getTextStyle(color, font, fontWeight)}> {text} </Text>
+    return (
+        <Text style={getTextStyle(color, font, fontWeight, fontfamilly)}>
+            {text}
+        </Text>
+    )
 }
 
 const styles = StyleSheet.create({
