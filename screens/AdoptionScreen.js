a2ce4268ad45236ca52e25777d69103af92c4fbf
Aikru <urkia.p@free.fr>
2021-10-08 02:08:51 +0200

    [screen/Adoption]integration

--- screens/AdoptionScreen.js
+++ screens/AdoptionScreen.js
@@ -1,10 +1,27 @@
-import React from "react";
-import {Text} from "react-native";
+import React from 'react'
+import { Button, Image, ScrollView, Text, View } from 'react-native'
+import ArchCounter from '../componants/ui/ArchCounter.js'
+import CustomButton from '../componants/ui/CustomButton.js'
+import CustomText from '../componants/ui/CustomText.js'
+import Pack from '../componants/ui/Pack.js'
 
 export default function AdoptionScreen() {
-    return(
-        <Text>
-            Adoption
-        </Text>
+    return (
+        <ScrollView>
+            <ArchCounter />
+            <View style={{ paddingHorizontal: 20 }}>
+                <CustomText fontWeight="bold" font={24}>
+                    Adoption
+                </CustomText>
+                <Image source={require('../assets/img/pack-asso.png')} />
+
+                <CustomButton
+                    color="white"
+                    bgColor="#F06D76"
+                    text="ACHETER UNE BOX"
+                />
+                <Pack />
+            </View>
+        </ScrollView>
     )
 }
